d127fdfe7ca5ec018dba2b303bd565d177ab2c2b
bukaman5 <131355554+bukaman5@users.noreply.github.com>
2023-04-27 17:43:15 +0300

    Init commit

---
+++ src/test/java/com/ski/skiresort/SkiResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ski.skiresort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
