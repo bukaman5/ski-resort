be40d0120000d0de8d1247a356e2c558c90fe6b0
bukaman5 <131355554+bukaman5@users.noreply.github.com>
2023-04-27 18:19:40 +0300

    Add new entities

---
+++ src/main/java/com/ski/skiresort/domain/entity/Coach.java
@@ -0,0 +1,48 @@
+package com.ski.skiresort.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "coach")
+public class Coach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "full_name")
+    private String fullName;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "visitor_coach",
+            joinColumns = @JoinColumn(name = "coach_id"),
+            inverseJoinColumns = @JoinColumn(name = "visitor_id")
+    )
+    private List<Visitor> visitors;
+    @OneToMany(mappedBy = "coach", cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<SkiPass> skiPasses;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "photo")
+    private String photo;
+}
